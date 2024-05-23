e6d86f11327c17795dfde04c6a64434f08c16060
Xiayucheng1212 <x1204585568@gmail.com>
2024-05-23 23:02:39 +0800

    Learning lecture 2

---
+++ Lecture2/vote-count-5.go
@@ -0,0 +1,36 @@
+package main
+
+import "time"
+import "math/rand"
+
+func main() {
+	rand.Seed(time.Now().UnixNano())
+
+	count := 0
+	finished := 0
+	ch := make(chan bool)
+	for i := 0; i < 10; i++ {
+		go func() {
+			ch <- requestVote()
+		}()
+	}
+	for count < 5 && finished < 10 {
+		// 這裡是main thread 順序執行， 一次拿一個值出來
+		v := <-ch
+		if v {
+			count += 1
+		}
+		finished += 1
+	}
+	if count >= 5 {
+		println("received 5+ votes!")
+
+	} else {
+		println("lost")
+	}
+}
+
+func requestVote() bool {
+	time.Sleep(time.Duration(rand.Intn(100)) * time.Millisecond)
+	return rand.Int()%2 == 0
+}
---
+++ condvar/README.md
@@ -0,0 +1,3 @@
+# Lecture 2 RPC and Threads
+
+Vote examples in GO to illustrate the interactions between threads.
