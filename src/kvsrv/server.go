5d0a1ee60d3f7e0ae1626ebfb4be7ada2405324c
Xiayucheng1212 <x1204585568@gmail.com>
2024-05-25 22:30:50 +0800

    Add Server part

--- src/kvsrv/server.go
+++ src/kvsrv/server.go
@@ -25,20 +25,46 @@ type KVServer struct {
 
 func (kv *KVServer) Get(args *GetArgs, reply *GetReply) {
 	// Your code here.
+	key := args.Key
+	if value, ok := kv.kvMap[key]; ok {
+		reply.Value = value
+	} else {
+		reply.Value = ""
+	}
 }
 
 func (kv *KVServer) Put(args *PutAppendArgs, reply *PutAppendReply) {
 	// Your code here.
+	key := args.Key
+	value := args.Value
+
+	kv.mu.Lock()
+	kv.kvMap[key] = value
+	kv.mu.Unlock()
+
+	reply.Value = value
 }
 
 func (kv *KVServer) Append(args *PutAppendArgs, reply *PutAppendReply) {
 	// Your code here.
+	key := args.Key
+	value := args.Value
+
+	kv.mu.Lock()
+	if oldValue, ok := kv.kvMap[key]; ok {
+		value = oldValue + value
+	}
+	kv.kvMap[key] = value
+	kv.mu.Unlock()
+
+	reply.Value = value
 }
 
 func StartKVServer() *KVServer {
 	kv := new(KVServer)
 
 	// You may need initialization code here.
+	kv.kvMap = make(map[string]string)
 
 	return kv
 }
